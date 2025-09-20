a6810b2523120f38d81eba9e60b204da084355ae
Ronja Gröhn <ronja.grohn@student.lab.fi>
2025-09-20 18:35:10 +0300

    Blink led external

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from machine import Pin
 import time
 
-led = Pin("LED", Pin.OUT)
+led = Pin(15, Pin.OUT)
 
 while True:
-    led.value(1)      # Turn LED on
-    time.sleep(1)     # Wait 1 second
-    led.value(0)      # Turn LED off
-    time.sleep(1)     # Wait 1 second
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
