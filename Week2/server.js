bb383c6a6efe563667d40a417989db49e27cbb3b
Ronja Gröhn <ronja.grohn@student.lab.fi>
2025-10-01 21:09:36 +0300

    cleaning up code

--- Week2/server.js
+++ Week2/server.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 // GET endpoint
 app.get('/api/sensor', (req, res) => {
-res.json({
-temperature: 22.5,
-humidity: 55,
-status: "OK"
+    res.json({
+    temperature: 22.5,
+    humidity: 55,
+    status: "OK"
 });
 });
 app.listen(port, () => {
-console.log(`Server running at
-http://localhost:${port}`);
+    console.log(`Server running at
+    http://localhost:${port}`);
 });
